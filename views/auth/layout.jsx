2b7d14a7b297cf6fb9abe05afdb50e29ac989946
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-10-06 16:21:16 -0400

    updated head with meta tag on auth layout

--- views/auth/layout.jsx
+++ views/auth/layout.jsx
@@ -9,6 +9,7 @@ class Layout extends React.Component {
           <link rel="stylesheet" href="/css/style.css"/>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous"></link>
           <script src="/js/app.js"></script>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         </head>
         <body>
           <header>
